bdeb85a8d576a589944425280c4277ffcb979eb1
AbhinavGupta-de <abhinav.gupta@outlook.de>
2023-08-11 16:37:22 +0530

    Finally Got AuthContext Working

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,33 +7,36 @@ class Navbar extends React.Component {
 	render() {
 		const { isDarkTheme, lightTheme, darkTheme } = this.context;
 		const theme = isDarkTheme ? darkTheme : lightTheme;
-		const { isAuthenticated, toggleAuth } = AuthContext;
 
 		return (
-			<nav
-				style={{
-					background: theme.background,
-					height: '120px',
-				}}
-			>
-				<h2
-					className="ui centered header"
-					style={{ textAlign: 'center', color: theme.text }}
-				>
-					Home
-				</h2>
-				<p
-					onClick={toggleAuth}
-					style={{ color: theme.text, textAlign: 'center' }}
-				>
-					{isAuthenticated ? 'Logged in' : 'Logged Out'}
-				</p>
-				<div className="ui three buttons">
-					<button className="ui button">Overview</button>
-					<button className="ui button">Contact</button>
-					<button className="ui button">Support</button>
-				</div>
-			</nav>
+			<AuthContext.Consumer>
+				{(context) => (
+					<nav
+						style={{
+							background: theme.background,
+							height: '120px',
+						}}
+					>
+						<h2
+							className="ui centered header"
+							style={{ textAlign: 'center', color: theme.text }}
+						>
+							Home
+						</h2>
+						<p
+							onClick={context.toggleAuth}
+							style={{ color: theme.text, textAlign: 'center' }}
+						>
+							{context.isAuthenticated ? 'Logged in' : 'Logged Out'}
+						</p>
+						<div className="ui three buttons">
+							<button className="ui button">Overview</button>
+							<button className="ui button">Contact</button>
+							<button className="ui button">Support</button>
+						</div>
+					</nav>
+				)}
+			</AuthContext.Consumer>
 		);
 	}
 }
